dac86149012c1a444e6cedfa157cffd96d31c3fc
ubuntu <76729530@qq.com>
2012-05-15 19:41:54 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playweibo::Application.config.secret_token = '49e2de9614c2ffbdbfbdca26a11c70ff0d62b1d21f9b62540f4b8cb04d9d286ed681e9608477e999a9010cdb574bef5857f3bd7941dcd42a74192e5a5cf4cae9'
